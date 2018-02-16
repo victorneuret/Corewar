0a9b73a001f0dddcbc58841377d75736835310f9
yann <yann.sainson@epitech.eu>
2018-02-16 15:41:15 +0100

    - Reorganized the asm folder - Fix malloc in function conv_hex (int_to_hex.c)

--- src/asm/utils/int_to_hex.c
+++ src/asm/utils/int_to_hex.c
@@ -21,14 +21,16 @@ static char *zero_hex(void)
 char *conv_hex(int nb)
 {
 	char *str = NULL;
-	int len = 0;
+	int len = 1;
 	int tmp = nb;
 	int i = 0;
 
 	if (nb == 0)
 		return zero_hex();
-	while ((tmp = tmp / 10) != 0)
+	while (tmp != 0) {
+		tmp = tmp / 256;
 		len++;
+	}
 	str = malloc(sizeof(char) * len);
 	if (!str)
 		return NULL;
@@ -36,7 +38,7 @@ char *conv_hex(int nb)
 	for (; tmp != 0; i++) {
 		str[i] = tmp % 256;
 		tmp = tmp / 256;
-		str[i + 1] = '\0';
 	}
+	str[i] = '\0';
 	return (my_revstr(str));
 }
