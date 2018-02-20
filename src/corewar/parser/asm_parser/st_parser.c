5fa79c28b5c92be51106c77d1cc1ac7cefe67e3e
rasorn <victor.neuret@epitech.eu>
2018-02-20 10:10:23 +0100

    rename files for asm instruction parser

--- src/corewar/parser/asm_parser/st_parser.c
+++ src/corewar/parser/asm_parser/st_parser.c
@@ -7,9 +7,7 @@
 
 #include "corewar/parser/asm_parser.h"
 
-bool st_parser(__attribute__((unused)) token_t *token,
-	__attribute__((unused)) char *asm_token,
-	__attribute__((unused)) int *i)
+bool st_parser(token_t *token, char *asm_token, int *i)
 {
 	return true;
 }
