727cfb806f26af64985f967188fe9a5f353e3a24
rasorn <victor.neuret@epitech.eu>
2018-02-20 10:41:04 +0100

    - added test .cor and source file (.s) - live parser set args_type to 0

--- asm/src_champ/zork.s
+++ asm/src_champ/zork.s
@@ -1,15 +1,15 @@
 #
-# comment
-# another one
-# another one
+# zork.s for corewar
 #
-
+# Bob Bylan
+#
+# Sat Nov 10 03:24;30 2081
+#
 .name "zork"
 .comment "just a basic living prog"
 
 l2:
-sti r1,%:live,%1
-and r1,%0,r1
-
-live: live %1
-zjmp %:live
+ld %3,r4
+ld %400,r5
+live %6
+ld %256,r2
--- src/corewar/parser/asm_parser/live_parser.c
+++ src/corewar/parser/asm_parser/live_parser.c
@@ -7,14 +7,14 @@
 
 #include "corewar/parser/asm_parser.h"
 
-bool live_parser(__attribute__((unused)) token_t *token,
-	__attribute__((unused)) char *asm_token, int *i)
+bool live_parser(token_t *token, char *asm_token, int *i)
 {
 	token_t *tmp = token;
 
 	my_printf("%s\n", "live");
 	for (; tmp->next; tmp = tmp->next);
 	tmp->command = asm_token[*i];
+	tmp->args_type = 0;
 	tmp->nb_bytes = 1;
 	*i += 1;
 	if (!asm_arg_direct(tmp, asm_token, 0, i))
