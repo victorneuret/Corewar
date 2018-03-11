92d2db37c6c2323c35af58a4472955bd908dfdf8
yann <yann.sainson@epitech.eu>
2018-03-11 16:27:19 +0100

    - Added function is_label in asm/utils

---
+++ include/asm/utils/is_label.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2018
+** CPE_corewar_2017
+** File description:
+** is_label.c
+*/
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "common/utils/str/str_utils.h"
+
+bool is_label(char const *str);
---
+++ src/asm/utils/is_label.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2018
+** CPE_corewar_2017
+** File description:
+** is_label.c
+*/
+
+#include "asm/utils/is_label.h"
+
+bool is_label(char const *str)
+{
+	size_t i = 0;
+
+	for (i = 0; str[i] != '\0' &&
+	(str[i] == ' ' || str[i] == '\t'); i++);
+	if (str[i] == '%' && str[i + 1] == ':')
+		return true;
+	else if (str[my_strlen(str) - 1] == ':')
+		return true;
+	return false;
+}
