a3ea5570e7e9acc058312eea58b69eb0768d8cd2
yann <yann.sainson@epitech.eu>
2018-03-11 18:13:44 +0100

    - Fixed unused parameter in add_asm

--- src/asm/functions/lfork_asm.c
+++ src/asm/functions/lfork_asm.c
@@ -9,6 +9,7 @@
 
 bool lfork_asm(int fd, char const *args, uint32_t *new_len,
 __attribute__((unused)) label_t *label_s)
+{
 	uint8_t i = 15;
 	int16_t value = 0;
 	char *str = substring(args, 1, my_strlen(args));
