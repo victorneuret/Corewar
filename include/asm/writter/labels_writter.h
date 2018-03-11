9db6c4fc4d285d0acb49ebbb3be191e943945193
shellbear <antoine.ordonez@epitech.eu>
2018-03-11 18:14:54 +0100

    - Create new functions for labels - Free label structure

---
+++ include/asm/writter/labels_writter.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2018
+** CPE_corewar_2017
+** File description:
+** Writter function_writter
+*/
+
+#pragma once
+
+#include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "asm/asm_struct.h"
+#include "common/utils/str/str_utils.h"
+
+void free_struct(label_t *label_s, size_t label_count);
+void insert_label_call(char *label, uint32_t call,label_t *label_s);
+void insert_label_define(uint32_t *new_len, label_t *label_s);
+void init_labels(label_t *label_s, asm_t *asm_s);
