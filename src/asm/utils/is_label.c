ede01f6e5b162450daa3448247abf4bb0af26a37
HOUIS Maxime <maxime.houis@epitech.eu>
2018-03-11 18:28:24 +0100

    Fixed valgrind error in is_label.c

--- src/asm/utils/is_label.c
+++ src/asm/utils/is_label.c
@@ -12,7 +12,9 @@ bool is_label(char const *str)
 	size_t i = 0;
 
 	for (i = 0; str[i] != '\0' &&
-	(str[i] == ' ' || str[i] == '\t'); i++);
+		(str[i] == ' ' || str[i] == '\t'); i++);
+	if (!str[i])
+		return false;
 	if (str[i] == '%' && str[i + 1] == ':')
 		return true;
 	else if (str[my_strlen(str) - 1] == ':')
